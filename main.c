e293265d05b96ba580657c4c5b77c7f937db1aa7
Hamza Dazia <hdazia@e1r4p6.1337.ma>
2025-01-11 19:02:16 +0100

    ss

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "push_swap.h"
+# include <unistd.h>    
+
+
+
+int main(int argc, char **argv)
+{
+    if(argc == 1)
+        return(write(2,"Error",5));
+    return 0;
+    int i = 1;
+
+    while(i <= argc)
+    {
+        check
+        i++;
+    }
+}
---
+++ push_swap.h
@@ -0,0 +1,11 @@
+#ifndef PUSH_SWAP
+#define PUSH_SWAP
+# include <unistd.h>
+
+typedef struct list
+{
+    int cont;
+    struct list *next;    
+} node;
+
+#endif
