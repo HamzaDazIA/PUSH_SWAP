121f7b68b93abdcf9a87212e2ce40646fb56b107
“HamzaDazAI” <hamzadaziag@gmail.com>
2025-01-31 16:21:03 +0100

    add same mod

--- checker_bonus/checker_bonus.c
+++ checker_bonus/checker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hdazia <hdazia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 10:58:14 by hdazia            #+#    #+#             */
-/*   Updated: 2025/01/31 13:43:36 by hdazia           ###   ########.fr       */
+/*   Updated: 2025/01/31 16:15:58 by hdazia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,31 +59,39 @@ static void	put_elemen_stack(t_stack **stack, char **pointer)
 	}
 	free_split(pointer);
 }
+int	applying_rules(t_stack **stack_a, t_stack **stack_b, char *str_input)
+{
+	int	size_b;
+	int	size_a;
+
+	size_a = 
+	str_input = get_next_line(0);
+	while (str_input)
+	{
+		do_this_rules(stack_a, stack_b, str_input);
+		
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	**stack_a;
 	t_stack	**stack_b;
 	char	**pointer;
-
-	if (argc > 1)
-	{
-		pointer = ft_check_input_bonus(argc, argv);
-		if (pointer)
-			return (6);
-		stack_a = malloc(sizeof(t_stack *));
-		if (stack_a == NULL)
-			return (free_split(pointer), 1);
-		stack_b = malloc(sizeof(t_stack *));
-		if (stack_b == NULL)
-			return (free(stack_a), free_split(pointer), 1);
-		*stack_a = NULL;
-		*stack_b = NULL;
-		put_elemen_stack(stack_a, pointer);
-        
-		free_element_stack(stack_a);
-		free_element_stack(stack_b);
-		free(stack_a);
-		free(stack_b);
-	}
-	return (0);
+	char	*str_input;
+	
+	if (argc < 2 )
+		exit(1);
+	pointer = ft_check_input_bonus(argc, argv);
+	stack_a = malloc(sizeof(t_stack *));
+	if (stack_a == NULL)
+		return (free_split(pointer), 1);
+	stack_b = malloc(sizeof(t_stack *));
+	if (stack_b == NULL)
+		return (free(stack_a), free_split(pointer), 1);
+	*stack_a = NULL;
+	*stack_b = NULL;
+	put_elemen_stack(stack_a, pointer);
+	applying_rules(stack_a, stack_b, str_input);
+	return (free_element_stack(stack_a), free_element_stack(stack_b), free(stack_a), free(stack_b), 0);
 }
